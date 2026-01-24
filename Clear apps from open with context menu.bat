fc5a80a8a5a64fcb25c22f30aa494d9914c3d95c
DXZ <biggoronswordfigher@gmail.com>
2026-01-24 16:35:27 -0600

    Add files via upload

---
+++ Clear apps from open with context menu.bat
@@ -0,0 +1,31 @@
+@echo off
+
+set APPS=flips.exe FlipsAutoPatcher.exe FlipsAutoPatcher-v2.0.0.exe
+
+echo Removing Flips entries from Windows 11 Open With...
+echo.
+
+for %%A in (%APPS%) do (
+
+  echo Removing %%A ...
+
+  :: App Paths (WIN11 PRIMARY SOURCE)
+  reg delete "HKCU\Software\Microsoft\Windows\CurrentVersion\App Paths\%%A" /f >nul 2>&1
+  reg delete "HKLM\Software\Microsoft\Windows\CurrentVersion\App Paths\%%A" /f >nul 2>&1
+
+  :: Applications
+  reg delete "HKCU\Software\Classes\Applications\%%A" /f >nul 2>&1
+  reg delete "HKLM\Software\Classes\Applications\%%A" /f >nul 2>&1
+  reg delete "HKCR\Applications\%%A" /f >nul 2>&1
+
+  :: OpenWithProgids (all files)
+  reg delete "HKCR\*\OpenWithProgids" /v %%A /f >nul 2>&1
+
+)
+
+:: Clear Explorer cache
+taskkill /f /im explorer.exe >nul
+start explorer.exe
+
+echo Done. Entries should now be GONE.
+pause
